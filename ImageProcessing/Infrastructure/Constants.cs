315959346d62372dfcb3a69482aa00c0d14d39ed
Nikolay <diksito@hotmail.com>
2014-03-19 21:21:11 +0200

    Added class with constants

---
+++ ImageProcessing/Infrastructure/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageProcessing.Infrastructure
+{
+    class Constants
+    {
+        public const string ERROR = "ERROR: ";
+        public const string TEMPORARY_FILE_PREFIX = "temp_";
+    }
+}
