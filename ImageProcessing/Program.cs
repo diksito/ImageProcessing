95d405d7b6bf2f4081dd029846d17de3d500ca1a
Nikolay Angelov <nikolay.angelov.angelov@gmail.com>
2014-04-18 19:56:49 +0300

    Fix: execution flow - resize, rotate and save as options are working

--- ImageProcessing/Program.cs
+++ ImageProcessing/Program.cs
@@ -30,39 +30,55 @@ namespace ImageProcessing
 
 
                 bool setDestinationPath = false;
-                string destinationPath = string.Empty;
+                string destinationPath, parameter = string.Empty;
                 if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_DEST, out destinationPath))
                     setDestinationPath = true;
 
                 // Resize image
-                string resizeArgs = string.Empty;
-                if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_RESIZE, out resizeArgs))
+                parameter = string.Empty;
+                if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_RESIZE, out parameter))
                 {
-                    //List<int> parameterValues = ArgumentsParser.ParseIntValues(item.Value);
-                    //image.Resize(parameterValues[0], parameterValues[1]);
-                    Log.Info("Resizing...");
-                    Log.Info("Value: " + resizeArgs);
+                    List<int> parameterValues = ArgumentsParser.ParseIntValues(parameter);
+                    if (parameterValues != null && parameterValues.Count == 2)
+                    {
+                        // Only positive pixels
+                        if (parameterValues[0] > 0 && parameterValues[1] > 0)
+                        {
+                            Log.Info("Resizing...");
+                            image.Resize(parameterValues[0], parameterValues[1]);
+                        }
+                    }
                 }
                 // Crop image
-                string cropArgs = string.Empty;
-                if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_CROP, out cropArgs))
+                parameter = string.Empty;
+                if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_CROP, out parameter))
                 {
                     // Croping image...
                     Log.Info("Croping...");
-                    Log.Info("Value: " + cropArgs);
-
-                    image.SaveAs(Utility.GenerateRandomFileName());
-
-                    //string saveTo = string.Empty;
-                    //if (setDestinationPath)
-                    //    destinationPath = Utility.CleanPath(destinationPath);
-                    //else
-                    //    destinationPath = Utility.CleanPath(image.CurrentDirectory) + @"\" + Utility.GenerateRandomFileName();
-                    
-                    //image.Crop(10, 30, 40, 50, destinationPath);
-
-
+                    Log.Info("Value: " + parameter);
                 }
+
+
+                // Rotate
+                parameter = string.Empty;
+                if (ArgumentsParser.Commands.TryGetValue(Constants.COMMAND_ROTATE, out parameter))
+                {
+                    int degrees;
+                    if(int.TryParse(parameter, out degrees))
+                    {
+                        image.Rotate(degrees);
+                        Log.Info("Rotating...");
+                    }
+                }
+
+                #region Save image
+                Log.Info("Saving...");
+                if (setDestinationPath)
+                    image.SaveAs(destinationPath);
+                else
+                    image.Save();
+                #endregion
+
 
                 // Free used resources
                 image.Dispose();
